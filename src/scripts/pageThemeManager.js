1f8d7f68c1e5aa97a678c26991c1389a11548e8a
Popeydanielmate <14269@coderacademy.edu.au>
2024-05-24 10:27:18 +1000

    feat: page theme toggle implemented

---
+++ src/scripts/pageThemeManager.js
@@ -0,0 +1,38 @@
+
+// Update #pageThemeHeading text
+function updatePageThemeHeading(){
+    let headingToUpdate = document.getElementById("pageThemeHeading");
+    headingToUpdate.textContent = getStoredPageTheme();
+}
+
+// Update #pageThemeButton text
+function updatePageThemeButtonText(){
+    let buttonToUpdate = document.getElementById("pageThemeButton");
+
+    let textToShow = "";
+
+    if (getStoredPageTheme() == "dark"){
+        textToShow = "Toggle to light mode";
+    } else {
+        textToShow = "Toggle to dark mode";
+    }
+
+    buttonToUpdate.textContent = textToShow;
+
+}
+
+
+// Add onclick to #pageThemeButton
+function togglePageTheme(){
+    if (getStoredPageTheme() == "dark"){
+       pageTheme = "light";
+    } else {
+        pageTheme = "dark";
+    }
+    setPageThemeToStorage();
+    updatePageThemeButtonText();
+    updatePageThemeHeading();
+}
+
+let pageThemeToggleButton = document.getElementById("pageThemeButton");
+pageThemeToggleButton.addEventListener("click", togglePageTheme);
